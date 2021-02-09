93ec4c9d7c899adc81e34da00deb835c9dd1d3d0
ZORDUS <74714905+ZORDUS@users.noreply.github.com>
2021-02-09 16:20:33 +0300

    Update Task4.cpp

--- practice_2/Task4/Task4.cpp
+++ practice_2/Task4/Task4.cpp
@@ -7,22 +7,22 @@ int main() {
     double x1, x2;
     double discr;
     
-    cout << "Enter a:";
+    cout << "Enter a: ";
     cin >> a;
-    cout << "Enter b:";
+    cout << "Enter b: ";
     cin >> b;
-    cout << "Enter c:";
+    cout << "Enter c: ";
     cin >> c;
 
     discr = b * b - 4 * a * c;
     
     if (discr < 0)
-        cout << "Error";
+        cout << "discr is negative";
     
     x1 = (-b + sqrt(discr)) / (2 * a);
     x2 = (-b - sqrt(discr)) / (2 * a);
     
-    cout << "x1 равняется:" << " " << x1 << endl;
-    cout << "x2 равняется:" << " " << x2 << endl;
+    cout << "x1 Г°Г ГўГ­ГїГҐГІГ±Гї:" << " " << x1 << endl;
+    cout << "x2 Г°Г ГўГ­ГїГҐГІГ±Гї:" << " " << x2 << endl;
     return 0;
 }
