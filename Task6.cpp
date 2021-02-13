f63b82551cb533b7efed8bfe8ef43a69267267cc
Sergey Dobrovolsky <dobrovols@gmail.com>
2021-02-13 20:53:18 +0300

    Polish Task6 source code

--- Task6.cpp
+++ Task6.cpp
@@ -1,28 +1,31 @@
 #include <iostream>
 #include <math.h>
-int main() 
+
+int main()
 {
 	using namespace std;
-	
+
 	setlocale(LC_ALL, "rus");
+
 	double w, x, a;
-	cout << "для вычисления значения 'w' введеите переменные";
-	cout << "Введите 'x':";
+	cout << "для вычисления значения 'w' введеите переменные" << endl;
+	cout << "Введите 'x': ";
 	cin >> x;
-	cout << "Введите 'a':";
+	cout << "Введите 'a': ";
 	cin >> a;
-	
+
 	if (x < 1)
 	{
 		w = a * log(abs(x));
-		cout << "Значения 'w':" << w;
+		cout << "Значение 'w': " << w << endl;
 	}
 	else if (a - x * x >= 0)
 	{
 		w = sqrt(a - x * x);
-		cout << "Значение 'w':" << w;
+		cout << "Значение 'w': " << w << endl;
 	}
 	else
-		cout << "Нарушение логики";
+		cout << "Нарушение логики" << endl;
+
 	return 0;
 }
