dcbe7f5f5d89404edd4a67ee62ddf465f3aac183
Sergey Dobrovolsky <dobrovols@gmail.com>
2021-02-13 20:55:14 +0300

    Small code style changes

--- Task7.cpp
+++ Task7.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+
 int main()
 {
 	using namespace std;
+
 	setlocale(LC_ALL, "RUS");
+
 	int a, b, f, x;
 	cout << "Введите цифры a, b , f: " << endl;
-	cout << "a= ";
+	cout << "a = ";
 	cin >> a;
-	cout << "b= ";
+	cout << "b = ";
 	cin >> b;
-	cout << "f= ";
+	cout << "f = ";
 	cin >> f;
+
 	cout << "Вычисляем по формуле: x=(a + b - f / a) + f * a * a - (a + b)" << endl;
 	x = (a + b - f / a) + f * a * a - (a + b);
-	cout << "x= " << x << endl;
+	cout << "x = " << x << endl;
+
 	return 0;
 }
 /*Запрограммировать следующее выражение: (а + b — f / а) + f * a * a — (a + b) Числа а, b, f вводятся с клавиатуры. Организовать пользовательский интерфейс, таким образом, чтобы было понятно, в каком порядке должны вводиться числа.
