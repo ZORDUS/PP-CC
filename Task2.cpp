9d228b419e463067a8eeceb0b243638bef5be393
Sergey Dobrovolsky <dobrovols@gmail.com>
2021-02-13 20:34:08 +0300

    Polish Task2

--- Task2.cpp
+++ Task2.cpp
@@ -3,16 +3,18 @@
 int main()
 {
     using namespace std;
-    float a, b;
+
+    double a, b;
     cout << "A = ";
     cin >> a;
     cout << "B = ";
     cin >> b;
-    cout << "Summa: " << a + b << endl
-        << "Raznost: " << a - b << endl
-        << "Proizvedenie: " << a * b << endl;
+
+    cout << "Sum: " << a + b << endl
+         << "Вifference: " << a - b << endl
+         << "Product: " << a * b << endl;
     if (b == 0)
         cout << "Division by zero!";
     else
-        cout << "Chastnoe: " << a / b << endl;
+        cout << "Quotient: " << a / b << endl;
 }
